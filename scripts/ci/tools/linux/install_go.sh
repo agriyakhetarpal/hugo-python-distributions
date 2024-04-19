0645456302326cf124cb8c06a2cc7ba510ca9bbe
arjxn.py <arjxn.py@gmail.com>
2024-04-19 12:05:35 +0530

    ↩️ Revert changes in `install_go.sh` & remove `install_zig.sh`

--- scripts/ci/tools/linux/install_go.sh
+++ scripts/ci/tools/linux/install_go.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+# !#/bin/bash
 
 # Small script to install Golang into a PyPA manylinux2014 Docker container
 
-apt-get update && apt-get install -y wget
+yum install -y wget
 
 arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
-    tarball="go1.22.1.linux-amd64.tar.gz"
-elif [ "$arch" == "aarch64" ] || [ "$arch" == "arm64" ]; then
-    tarball="go1.22.1.linux-arm64.tar.gz"
+    tarball="go1.22.2.linux-amd64.tar.gz"
+elif [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
+    tarball="go1.22.2.linux-arm64.tar.gz"
 else
     echo "Unsupported architecture: $arch"
     exit 1
