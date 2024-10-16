bc807799947c8ee88f62913d18bd3def4383c111
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-10-16 21:46:25 +0530

    🎬 Set CGO compilation flags to strip binaries

--- setup.py
+++ setup.py
@@ -200,6 +200,10 @@ class HugoBuilder(build_ext):
         if zig_target:
             os.environ["CC"] = f"zig cc -target {zig_target}"
             os.environ["CXX"] = f"zig c++ -target {zig_target}"
+            # Add additional flags to the linker to ensure that the binary is
+            # stripped of debug information and is as small as possible for release
+            os.environ["CGO_CFLAGS"] = "-g0 -O3 -ffunction-sections -fdata-sections"
+            os.environ["CGO_LDFLAGS"] = "-s -w -Wl,--gc-sections"
         else:
             print(f"Warning: No Zig target found for GOOS={goos} and GOARCH={goarch}")
 
