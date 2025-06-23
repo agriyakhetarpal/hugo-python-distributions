724260549ad58c008129c13ff7d5b3cc8bfcd9c3
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-06-23 14:39:02 +0530

    9️⃣ Bump to Hugo 0.147.9

--- hugo/cli.py
+++ hugo/cli.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 from sys import platform as sysplatform
 
-HUGO_VERSION = "0.147.8"
+HUGO_VERSION = "0.147.9"
 FILE_EXT = ".exe" if sysplatform == "win32" else ""
 HUGO_PLATFORM = {"darwin": "darwin", "linux": "linux", "win32": "windows"}[sysplatform]
 
