42f441a6509acb794e973ada03fca89f9a16b421
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-04-25 19:40:45 +0530

    Update CLI paths for Hugo executable

--- src/hugo/cli.py
+++ src/hugo/cli.py
@@ -7,24 +7,23 @@ hugo: Binaries for the Hugo static site generator, installable with pip
 from __future__ import annotations
 
 import os
-import sysconfig
+from pathlib import Path
 from sys import platform as sysplatform
 
 from hugo._version import HUGO_VERSION
 
-FILE_EXT = ".exe" if sysplatform == "win32" else ""
-
-HUGO_EXECUTABLE = os.path.join(  # noqa: PTH118
-    sysconfig.get_path("scripts"), "_hugo_bin" + FILE_EXT
-)
+HUGO_EXECUTABLE = "hugo.exe" if sysplatform == "win32" else "hugo"
+HUGO_EXECUTABLE_PATH = Path(__file__).with_name("binaries") / HUGO_EXECUTABLE
 
 
 def __call():
     """
     Hugo binary entry point. Passes all command-line arguments to Hugo.
     """
+    hugo_executable_str = os.fspath(HUGO_EXECUTABLE_PATH)
+
     print(
-        f"\033[95mRunning Hugo {HUGO_VERSION} via hugo-python-distributions at {HUGO_EXECUTABLE}\033[0m"
+        f"\033[95mRunning Hugo {HUGO_VERSION} via hugo-python-distributions at {hugo_executable_str}\033[0m"
     )
 
     from sys import argv as sysargv
@@ -32,9 +31,9 @@ def __call():
     if sysplatform == "win32":
         from subprocess import check_call
 
-        check_call([HUGO_EXECUTABLE, *sysargv[1:]])
+        check_call([hugo_executable_str, *sysargv[1:]])
     else:
-        os.execv(HUGO_EXECUTABLE, ["hugo", *sysargv[1:]])
+        os.execv(hugo_executable_str, ["hugo", *sysargv[1:]])
 
 
 if __name__ == "__main__":
