21577e783f58814e1019e3b0166a4b5300350b1a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-04-25 15:01:48 +0530

    Just make the binary "hugo" or "hugo.exe"

--- src/hugo/cli.py
+++ src/hugo/cli.py
@@ -7,59 +7,15 @@ hugo: Binaries for the Hugo static site generator, installable with pip
 from __future__ import annotations
 
 import os
+import sysconfig
 from sys import platform as sysplatform
 
-HUGO_VERSION = "0.160.1"
+from hugo._version import HUGO_VERSION
+
 FILE_EXT = ".exe" if sysplatform == "win32" else ""
-if sysplatform == "win32":
-    HUGO_PLATFORM = "windows"
-elif sysplatform == "linux":
-    HUGO_PLATFORM = "linux"
-else:
-    HUGO_PLATFORM = "darwin"
-
-
-def get_hugo_arch():
-    from sys import maxsize as sysmaxsize
-
-    if sysplatform == "win32":
-        from platform import machine
-
-        m = machine()
-    else:
-        m = os.uname().machine
-
-    HUGO_ARCH = {
-        "x86_64": "amd64",
-        "arm64": "arm64",
-        "AMD64": "amd64",
-        "aarch64": "arm64",
-        "x86": "386",
-        "i686": "386",
-        "i386": "386",
-        "s390x": "s390x",
-        "ppc64le": "ppc64le",
-        "armv7l": "arm",
-        "armv6l": "arm",
-        "riscv64": "riscv64",
-    }[m]
-
-    # platform.machine returns AMD64 on Windows because the architecture is
-    # 64-bit (even if one is running a 32-bit Python interpreter). Therefore
-    # we use sys.maxsize to handle this special case on Windows
-
-    if not (sysmaxsize > 2**32) and sysplatform == "win32":
-        HUGO_ARCH = "386"
-
-    return HUGO_ARCH
-
-
-HUGO_ARCH = get_hugo_arch()
-
-DIR = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
 
 HUGO_EXECUTABLE = os.path.join(  # noqa: PTH118
-    DIR, "binaries", f"hugo-{HUGO_VERSION}-{HUGO_PLATFORM}-{HUGO_ARCH}{FILE_EXT}"
+    sysconfig.get_path("scripts"), "_hugo_bin" + FILE_EXT
 )
 
 
